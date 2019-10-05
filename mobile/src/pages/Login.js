b3f31b9aeafea87e58a8bcbe3aa49b9deb2e3598
Dimas Junior <dimas.pedreira@imexperts.com.br>
2019-10-05 18:11:06 -0300

    Desenvolvendo a parte Mobile

---
+++ mobile/src/pages/Login.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, StyleSheet, Text, Image } from 'react-native';
+
+import logo from '../assets/logo.png';
+
+export default function Login() {
+  return (
+    <View style={styles.container}>
+      <Image source={logo} />
+
+      <Text style={styles.label} >SEU E-MAIL *</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  
+});
